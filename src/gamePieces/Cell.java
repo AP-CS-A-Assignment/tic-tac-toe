2c5c982966169e3dd7f4356e13dfa15981cdf0b5
DracoSeeker <58440497+DracoSeeker@users.noreply.github.com>
2020-05-27 19:25:42 -0400

    Continued commenting

--- src/gamePieces/Cell.java
+++ src/gamePieces/Cell.java
@@ -12,10 +12,25 @@ import main.GUI;
 
 public class Cell extends StackPane {
 
+    /**
+     * The image of the x, o, or empty
+     */
     private ImageView imageView;
+    /**
+     * Holds the position in the {@link Board} of the {@link Cell}
+     */
     private int row, col;
+    /**
+     * Holds the state of the {@link Cell}, i.e. x, o, empty
+     */
     private CellStates state;
 
+    /**
+     * Sets up the position of the cell, the {@link ImageView} in it, and the sizings
+     * @param _row row position
+     * @param _col column position
+     * @param container Size of the topmost definitely sized pane
+     */
     public Cell(int _row, int _col, Pane container) {
         row = _row;
         col = _col;
@@ -28,17 +43,19 @@ public class Cell extends StackPane {
         setPadding(new Insets(insets));
 
         imageView = new ImageView();
-//        setFitWidth(40);
-//        setFitHeight(40);
         imageView.fitHeightProperty().bind(imageView.fitWidthProperty());
         imageView.fitWidthProperty().bind(container.minWidthProperty().subtract(50 + insets * 6).divide(3.0));
         setImage();
 
-        setPickOnBounds(true);
+        //Calls onClick() when clicked
         setOnMouseClicked(event -> onClick(event));
         getChildren().add(imageView);
     }
 
+    /**
+     * Checks if its the player's turn and the cell is empty, then sets the image to an O rotated by some random 90 degree amount
+     * @param m The event from the click
+     */
     private void onClick(MouseEvent m) {
         if (state.equals(CellStates.EMPTY) && GUI.isLocalPLayersTurn()){
             state = CellStates.O;
@@ -48,33 +65,54 @@ public class Cell extends StackPane {
         }
     }
 
+    /**
+     * Acts as the way for the opponent to click on a cell
+     */
     public void click() {
         state = CellStates.X;
         setImage();
         setRotate(90 * (int)(Math.random() * 4));
     }
 
+    /**
+     * Empties the cell
+     */
     public void reset() {
         state = CellStates.EMPTY;
         setImage();
     }
 
+    /**
+     * Sets the image to the one that represents the current state
+     */
     private void setImage() {
         imageView.setImage(state.getImage());
     }
 
+    /**
+     * @return the row position of the Cell
+     */
     public int getRow() {
         return row;
     }
 
+    /**
+     * @return the column position of the cell
+     */
     public int getCol() {
         return col;
     }
 
+    /**
+     * @return the current state of the Cell
+     */
     public CellStates getState() {
         return state;
     }
 
+    /**
+     * @return String representation of the state
+     */
     @Override
     public String toString() {
         return state.toString();
--- src/gamePieces/CellStates.java
+++ src/gamePieces/CellStates.java
@@ -2,13 +2,29 @@ package gamePieces;
 
 import javafx.scene.image.Image;
 
+/**
+ * Represents the states of the Cells and holds the images for each state
+ */
 public enum CellStates {
+    /**
+     * Empty state
+     */
     EMPTY("Icons/Empty.png"),
+    /**
+     * Opponent state
+     */
     X("Icons/Ex.png"),
+    /**
+     * Local Player state
+     */
     O("Icons/Oh.png");
 
     private Image image;
 
+    /**
+     * Initializes the image in the state
+     * @param path path to the image
+     */
     CellStates(String path) {
         try {
             image = new Image(CellStates.class.getClassLoader().getResourceAsStream(path));
@@ -17,6 +33,9 @@ public enum CellStates {
         }
     }
 
+    /**
+     * @return The Image representing the state
+     */
     public Image getImage() {
         return image;
     }
